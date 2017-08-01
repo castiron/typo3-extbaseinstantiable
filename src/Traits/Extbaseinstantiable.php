efde7893e3524f187b986d74eb965a324a61cad2
Gabe Blair <gabe@castironcoding.com>
2017-08-01 15:00:35 -0700

    [:baby:] Initial commit

---
+++ src/Traits/Extbaseinstantiable.php
@@ -0,0 +1,28 @@
+<?php namespace Castiron\Extbaseinstantiable\Traits;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class ExtbaseInstantiable
+ * @package CIC\Cicbase
+ */
+trait ExtbaseInstantiable {
+    /**
+     * @return self
+     */
+    public static function get() {
+        $args = func_get_args();
+        array_unshift($args, get_called_class());
+        return call_user_func_array(
+            array(static::objectManager(), 'get'),
+            $args
+        );
+    }
+
+    /**
+     * @return \TYPO3\CMS\Extbase\Object\ObjectManager object
+     */
+    protected static function objectManager() {
+        return GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+    }
+}
